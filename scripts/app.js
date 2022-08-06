51318c350e9b9499be99c3854c280d44150b384f
ln-dev7 <leonelngoya@gmail.com>
2022-08-06 17:23:01 +0100

    header responsive

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,22 @@
+// Menu burger
+const headerMenu = document.querySelector(".header-menu");
+const menuOpen = document.querySelector(".menu-open");
+const menuClose = document.querySelector(".menu-close");
+const headerOverlay = document.querySelector(".header-overlay");
+
+menuOpen.addEventListener("click", function () {
+  setTimeout(function () {
+    headerMenu.classList.add("show");
+  }, 100);
+  headerOverlay.classList.add("show");
+});
+menuClose.addEventListener("click", function () {
+  headerMenu.classList.remove("show");
+  setTimeout(function () {
+    headerOverlay.classList.remove("show");
+  }, 300);
+});
+
 // Animation au scroll
 
 const ratio = 0.1;
